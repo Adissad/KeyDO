318407bdf04e5c752be3f8b4b9853e738e62c928
Mathias <ODYEK@MacBook-Air-de-Mathias.local>
2021-08-02 19:36:05 +0200

    MAJ

--- frontend/screens/AccMatchScreen.js
+++ frontend/screens/AccMatchScreen.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 
-export default function AccMathcScreen(props){
 
-    
+const [matchResult, setMatchResult] = useState(false);
+
+
+
+
+function AccMathcScreen(props) {
+
+    var Music = [DanceScreen, RnBScreen, AfroScreen, RockScreen, BluesScreen, HipHopScreen, LatinoScreen, JazzScreen, SoulScreen, ClassiqueScreen, ReaggaeScreen, PopScreen, FrenchScreen, KpopScreenScreen, MétalScreen, FunkScreen, FolkScreen, CountryScreen, DécenniesScreen, GospelScreen];
+
+    var GenreMusic = () => {
+
+        if (music.token == RnBScreen) {
+            setMatchResult(true);
+        } else {
+            setMatchResult(false);
+        }
+    }
+
+    return (
+
+        
+        
+    )
+
 
 }
+
+function mapDispatch(dispatch) {
+    return {
+        onSnap: function (match) {
+            dispatch({ type: 'matchResult', match })
+        }
+    }
+}
+
+export default connect(null, mapDispatch)(AccMathcScreen);
