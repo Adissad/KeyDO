fba57296aaa7b208ff2d2084ef14def80fcb4c12
Méwen D <dorafimewen@protonmail.com>
2021-08-04 19:46:33 +0200

    first commit fakechat

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,20 +1,5 @@
 let mongoose = require('mongoose');
 
-<<<<<<< HEAD
-let userSchema = mongoose.Schema({
-	// username: String,
-	name: String,
-	email: String,
-	password: String,
-	birthdayDate: Date,
-	gender: String,
-	city: String,
-	avatar: String,
-	picture: String,
-	token: String,
-	inscriptionDate: Date
-});
-=======
 var userSchema = mongoose.Schema({
     name : String,
     email : String,
@@ -27,7 +12,6 @@ var userSchema = mongoose.Schema({
     token : String,
     inscriptionDate : Date
 })
->>>>>>> ef09c9fe910b43963ccf0fb8857cf3a030e66fda
 
 let userModel = mongoose.model('users', userSchema);
 
